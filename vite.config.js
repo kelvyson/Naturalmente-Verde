d1ba84f2cdc093bfc540a42dbf2d4e2e471ccbb6
Kelvyson <kelvysondiegolucas@gmail.com>
2026-05-23 20:28:07 -0300

    fix vite base path

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Naturalmente-Verde/',
-  plugins: [react()]
+  plugins: [react()],
+  base: '/',
 })
